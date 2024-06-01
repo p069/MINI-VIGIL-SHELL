059bcb897af7a1203f5bfe558148541d8b5a1365
Proma Ray <124684642+p069@users.noreply.github.com>
2024-06-02 00:51:15 +0530

    Add files via upload

--- minivigil.sh
+++ minivigil.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
 
-follow_redirects() {
+check() {
   url="$1"
-  max_redirects="$2"
-  current_redirects=0
+  final_url=$(curl -sIL -w "%{url_effective}" -o /dev/null "$url")
 
-  while [ "$current_redirects" -lt "$max_redirects" ]; do
-
-    final_url=$(curl -sIL -o /dev/null -w "%{url_effective}\n" "$url")
-
-    echo "Redirect $((current_redirects + 1)): $url -> $final_url"
-
-    if [ "$url" == "$final_url" ]; then
-      break
-    fi
-
-    url="$final_url"
-    current_redirects=$((current_redirects + 1))
-  done
-
-  echo "Final Destination URL: $final_url"
+  if [ $? -eq 0 ]; then
+    echo "Entered URL : $url"
+    echo "Final Destination : $final_url"
+  else
+    echo "Failed to retrieve URL: $url"
+  fi
 }
 
-if [ $# -lt 1 ]; then
-  echo "Usage: $0 <URL> [max_redirects]"
-  exit 1
-fi
-
-url="$1"
-max_redirects="${2:-10}"
-
-follow_redirects "$url" "$max_redirects"
-
+check "$1"
